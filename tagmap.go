29d7ef6a27fc4e1f8c838df88dc18e466a0f0dc3
Matt Sherman <mwsherman@gmail.com>
2018-05-09 16:40:39 -0400

    Add comments

--- tagmap.go
+++ tagmap.go
@@ -25,11 +25,13 @@ func NewTagMap(d *Dictionary) *TagMap {
 	return result
 }
 
+// Dictionary is a structure for containing tags and synonyms, for easy passing around
 type Dictionary struct {
 	Tags     []string
 	Synonyms map[string]string
 }
 
+// NewDictionary instantiates a Dictionary pointer given tags and synonyms. Synonyms are a map of synonym (key) → canonical (value).
 func NewDictionary(tags []string, synonyms map[string]string) *Dictionary {
 	return &Dictionary{tags, synonyms}
 }
