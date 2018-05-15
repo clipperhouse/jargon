2740f9fbd482945f0134954a4b62c09d33571d4b
Matt Sherman <mwsherman@gmail.com>
2018-05-15 15:51:42 -0400

    Add some comments

--- token.go
+++ token.go
@@ -11,19 +11,22 @@ type Token struct {
 	space bool
 }
 
+// String is the string value of the token
 func (t Token) String() string {
 	return t.value
 }
 
-// IsPunct indicates that the token should
+// IsPunct indicates that the token should be considered 'breaking' of a run of words; a delimiter.
 func (t Token) IsPunct() bool {
 	return t.punct
 }
 
+// IsSpace indicates that the token consists entirely of white space (as defined by the unicode package)
 func (t Token) IsSpace() bool {
 	return t.space
 }
 
+// Join reconstructs a slice of tokens into their original string (assuming the tokens preserved fidelity of the original, esp white space!)
 func Join(tokens []Token) string {
 	joined := make([]string, 0)
 	for _, t := range tokens {
