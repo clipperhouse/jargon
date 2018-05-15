a55d604707fd9980bd2f8bab3049cc4850f71c60
Matt Sherman <mwsherman@gmail.com>
2018-05-15 18:33:07 -0400

    Whitespace tokenizer is not buying us anything, deprecate.

--- whitespace.go
+++
@@ -1,8 +0,0 @@
-package jargon
-
-import "unicode"
-
-// WhiteSpace is a tokenizer with whitespace as a delimiter (as defined in Go's `unicode` package)
-// It uses the definition of whitespace from the unicode package: https://golang.org/pkg/unicode/#IsSpace
-// Space are tokens and will be returned alongside words; it's up to the caller to ignore them if desired
-var WhiteSpace = &delimited{isDelimiter: unicode.IsSpace}
