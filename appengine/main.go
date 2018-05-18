bd8171c944a960e789f8a010a6237d6e7c251ffd
Matt Sherman <mwsherman@gmail.com>
2018-05-18 16:57:42 -0400

    Add appengine site

---
+++ appengine/main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"html/template"
+	"net/http"
+
+	"github.com/clipperhouse/jargon"
+
+	"google.golang.org/appengine"
+)
+
+func main() {
+	getTemplate()
+	http.HandleFunc("/", mainHandler)
+	appengine.Main()
+}
+
+var demo = `We might have some prose regarding Ruby on Rails or NODEJS, like a job description.
+
+Or some structured data:
+{
+    "language": "ObjC"
+}`
+
+func mainHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+
+	model := textModel{}
+
+	if r.Method == "POST" {
+		text := r.PostFormValue("text")
+		model.Original = text
+		model.Result = jargon.Lemmatize(text)
+	} else {
+		model.Original = demo
+	}
+
+	tmpl := getTemplate()
+	tmpl.Execute(w, model)
+}
+
+type textModel struct {
+	Original string
+	Result   string
+}
+
+var mainTmpl *template.Template
+
+func getTemplate() *template.Template {
+	if mainTmpl == nil {
+		t, err := template.ParseFiles("main.html")
+		if err != nil {
+			panic(err)
+		}
+		mainTmpl = t
+	}
+
+	return mainTmpl
+}
