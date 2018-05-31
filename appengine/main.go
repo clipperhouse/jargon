ade2803e99585fc0a1da1673f5e31f75352fd10b
Matt Sherman <mwsherman@gmail.com>
2018-05-31 10:23:01 -0400

    better routing

--- appengine/main.go
+++ appengine/main.go
@@ -11,34 +11,48 @@ import (
 )
 
 func main() {
-	http.HandleFunc("/jargon", mainHandler)
+	http.HandleFunc("/", mainHandler)
 	appengine.Main()
 }
 
 func mainHandler(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path != "/jargon" {
+	// CORS headers
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Headers", "X-Requested-With")
+
+	parts := strings.Split(r.URL.Path, "/")
+	if len(parts) == 1 {
 		http.NotFound(w, r)
 		return
 	}
 
-	w.Header().Set("Access-Control-Allow-Origin", "*")
-	w.Header().Set("Access-Control-Allow-Headers", "X-Requested-With")
+	switch route := parts[1]; {
+	case route == "text" || route == "jargon":
+		textHandler(w, r)
+	default:
+		http.NotFound(w, r)
+	}
+}
 
+func textHandler(w http.ResponseWriter, r *http.Request) {
 	text := r.PostFormValue("text")
-	tokens := jargon.TechProse.Tokenize(text)
 
-	lemmatized := jargon.StackExchange.LemmatizeTokens(tokens)
+	if len(strings.TrimSpace(text)) > 0 {
+		tokens := jargon.TechProse.Tokenize(text)
 
-	for t := range lemmatized {
-		if t.IsLemma() {
-			span.Execute(w, t)
-		} else {
-			w.Write([]byte(t.String()))
+		lemmatized := jargon.StackExchange.LemmatizeTokens(tokens)
+
+		for t := range lemmatized {
+			if t.IsLemma() {
+				lemma.Execute(w, t)
+			} else {
+				w.Write([]byte(t.String()))
+			}
 		}
 	}
 }
 
-var span = template.Must(template.New("span").Parse(`<span class="lemma">{{ .String }}</span>`))
+var lemma = template.Must(template.New("span").Parse(`<span class="lemma">{{ .String }}</span>`))
 
 func isAjax(r *http.Request) bool {
 	return strings.ToLower(r.Header.Get("X-Requested-With")) == "xmlhttprequest"
