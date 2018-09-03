47ad3120fa2d7832e18ed316c50468214f907f28
Matt Sherman <mwsherman@gmail.com>
2018-09-03 13:59:30 -0400

    Allow Stdin piping, alongside flags. Update usage.

--- cmd/jargon/main.go
+++ cmd/jargon/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bufio"
 	"flag"
+	"fmt"
 	"io"
 	"net/http"
 	"os"
@@ -24,7 +25,16 @@ func main() {
 	case len(u) > 0:
 		check(lemURL(u))
 	default:
-		flag.PrintDefaults()
+		// No flags? Check to see if piped, otherwise print help.
+		fi, err := os.Stdin.Stat()
+		check(err)
+
+		piped := (fi.Mode() & os.ModeCharDevice) == 0 // https://stackoverflow.com/a/43947435/70613
+		if piped {
+			check(lemStdin())
+		} else {
+			flag.Usage()
+		}
 	}
 }
 
@@ -41,6 +51,23 @@ func init() {
 	flag.StringVar(&f, "f", "", "A file path to lemmatize")
 	flag.StringVar(&s, "s", "", "A (quoted) string to lemmatize")
 	flag.StringVar(&u, "u", "", "A URL to fetch and lemmatize")
+	flag.Usage = func() {
+		cmd := os.Args[0]
+		out := flag.CommandLine.Output()
+
+		usage := `
+Usage: %[1]s accepts piped text from tools such as cat, curl or echo, via Stdin
+		
+  Example: echo "I luv Rails" | %[1]s
+
+Alternatively, use %[1]s 'standalone' by passing flags for text sources:
+
+`
+		fmt.Fprintf(out, usage, cmd)
+		flag.PrintDefaults()
+		fmt.Fprintf(out, "\n  Example: jargon -f /path/to/file.txt\n\n")
+		fmt.Fprintf(out, "Results are piped to Stdout (regardless of input)\n\n")
+	}
 }
 
 // turns out that buffering on the way out performs ~40% better, at least on my machine
@@ -69,6 +96,10 @@ func lemURL(u string) error {
 	defer resp.Body.Close()
 
 	return lem(resp.Body, w)
+}
+
+func lemStdin() error {
+	return lem(os.Stdin, w)
 }
 
 var lemmatizer = jargon.NewLemmatizer(stackexchange.Dictionary, 3)
