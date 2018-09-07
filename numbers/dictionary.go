8b972547255838a64100034db38a37ded7d249be
Matt Sherman <mwsherman@gmail.com>
2018-09-07 15:59:53 -0400

    First cut at number dictionary

---
+++ numbers/dictionary.go
@@ -0,0 +1,232 @@
+package numbers
+
+import (
+	"strconv"
+	"strings"
+)
+
+// Given a phrase like "three hundred thousand", return "300000"
+// The form is exactly one leading number followed by zero or more magnitudes
+// In the above example, "thirty five" is the number, "hundred" and "thousand" are magnitudes
+// All passed tokens must contribute to the number for the lookup to succeed
+
+// Test cases:
+// "three" => "3"
+// "3" => "3"
+// "3 thousand" => "3000"
+// "three thousand" => "3000"
+// "three hundred thousand" => "300000"
+// "thirty-five thousand" => "35000"
+// "a hundred" => not a specific number for our purpose
+// "thousand" => not a freestanding number for our purpose
+// "foo five hundred" => not a number
+// "two hundred fifty five" => out of scope for this dictionary (for now)
+
+type dictionary struct{}
+
+var Dictionary = &dictionary{}
+
+func (d *dictionary) Lookup(s []string) (string, bool) {
+	if len(s) == 0 {
+		return "", false
+	}
+	p := parser{tokens: s}
+	return p.Parse()
+}
+
+type parser struct {
+	tokens []string
+	ints   []int64
+	floats []float64
+	pos    int
+}
+
+func (p *parser) current() string {
+	return normalize(p.tokens[p.pos])
+}
+
+func (p *parser) Parse() (string, bool) {
+	p.pos = 0
+	first := p.current()
+
+	// Try parsing first token as int
+	i, err := strconv.ParseInt(first, 10, 64)
+	if err == nil {
+		p.ints = append(p.ints, i)
+		p.pos++
+		return p.parseMagnitudesInt()
+	}
+
+	// Try parsing first token as float
+	f, err := strconv.ParseFloat(first, 64)
+	if err == nil {
+		p.floats = append(p.floats, f)
+		p.pos++
+		return p.parseMagnitudesFloat()
+	}
+
+	// Try parsing first token as number word (int)
+	num, ok := numbers[first]
+	if ok {
+		p.ints = append(p.ints, num)
+		p.pos++
+		return p.parseMagnitudesInt()
+	}
+
+	return "", false
+}
+
+func normalize(s string) string {
+	result := s
+	result = strings.Replace(result, ",", "", -1)
+	result = strings.Replace(result, "-", "", -1)
+	return result
+}
+
+func (p *parser) parseMagnitudesInt() (string, bool) {
+	for p.pos < len(p.tokens) {
+		m, ok := magnitudes[p.current()]
+		if !ok {
+			return "", false
+		}
+		p.ints = append(p.ints, m)
+		p.pos++
+	}
+
+	result := int64(1) // identity
+	for i := 0; i < len(p.ints); i++ {
+		result = result * p.ints[i]
+	}
+
+	return strconv.FormatInt(result, 10), true
+}
+
+func (p *parser) parseMagnitudesFloat() (string, bool) {
+	for p.pos < len(p.tokens) {
+		m, ok := magnitudes[p.current()]
+		if !ok {
+			return "", false
+		}
+		p.floats = append(p.floats, float64(m))
+		p.pos++
+	}
+
+	result := float64(1) // identity
+	for i := 0; i < len(p.floats); i++ {
+		result = result * p.floats[i]
+	}
+
+	return strconv.FormatFloat(result, 'f', -1, 64), true
+}
+
+var numbers = map[string]int64{
+	"one":          1,
+	"two":          2,
+	"three":        3,
+	"four":         4,
+	"five":         5,
+	"six":          6,
+	"seven":        7,
+	"eight":        8,
+	"nine":         9,
+	"ten":          10,
+	"eleven":       11,
+	"twelve":       12,
+	"thirteen":     13,
+	"fourteen":     14,
+	"fifteen":      15,
+	"sixteen":      16,
+	"seventeen":    17,
+	"eighteen":     18,
+	"nineteen":     19,
+	"twenty":       20,
+	"twentyone":    21,
+	"twentytwo":    22,
+	"twentythree":  23,
+	"twentyfour":   24,
+	"twentyfive":   25,
+	"twentysix":    26,
+	"twentyseven":  27,
+	"twentyeight":  28,
+	"twentynine":   29,
+	"thirty":       30,
+	"thirtyone":    31,
+	"thirtytwo":    32,
+	"thirtythree":  33,
+	"thirtyfour":   34,
+	"thirtyfive":   35,
+	"thirtysix":    36,
+	"thirtyseven":  37,
+	"thirtyeight":  38,
+	"thirtynine":   39,
+	"forty":        40,
+	"fortyone":     41,
+	"fortytwo":     42,
+	"fortythree":   43,
+	"fortyfour":    44,
+	"fortyfive":    45,
+	"fortysix":     46,
+	"fortyseven":   47,
+	"fortyeight":   48,
+	"fortynine":    49,
+	"fifty":        50,
+	"fiftyone":     51,
+	"fiftytwo":     52,
+	"fiftythree":   53,
+	"fiftyfour":    54,
+	"fiftyfive":    55,
+	"fiftysix":     56,
+	"fiftyseven":   57,
+	"fiftyeight":   58,
+	"fiftynine":    59,
+	"sixty":        60,
+	"sixtyone":     61,
+	"sixtytwo":     62,
+	"sixtythree":   63,
+	"sixtyfour":    64,
+	"sixtyfive":    65,
+	"sixtysix":     66,
+	"sixtyseven":   67,
+	"sixtyeight":   68,
+	"sixtynine":    69,
+	"seventy":      70,
+	"seventyone":   71,
+	"seventytwo":   72,
+	"seventythree": 73,
+	"seventyfour":  74,
+	"seventyfive":  75,
+	"seventysix":   76,
+	"seventyseven": 77,
+	"seventyeight": 78,
+	"seventynine":  79,
+	"eighty":       80,
+	"eightyone":    81,
+	"eightytwo":    82,
+	"eightythree":  83,
+	"eightyfour":   84,
+	"eightyfive":   85,
+	"eightysix":    86,
+	"eightyseven":  87,
+	"eightyeight":  88,
+	"eightynine":   89,
+	"ninety":       90,
+	"ninetyone":    91,
+	"ninetytwo":    92,
+	"ninetythree":  93,
+	"ninetyfour":   94,
+	"ninetyfive":   95,
+	"ninetysix":    96,
+	"ninetyseven":  97,
+	"ninetyeight":  98,
+	"ninetynine":   99,
+}
+
+var magnitudes = map[string]int64{
+	"hundred":     1e2,
+	"thousand":    1e3,
+	"million":     1e6,
+	"billion":     1e9,
+	"trillion":    1e12,
+	"quadrillion": 1e15,
+	"quintillion": 1e15,
+}
---
+++ numbers/dictionary_test.go
@@ -0,0 +1,37 @@
+package numbers
+
+import "testing"
+
+type test struct {
+	input    []string
+	expected expected
+}
+
+type expected struct {
+	result string
+	ok     bool
+}
+
+func TestInts(t *testing.T) {
+	tests := []test{
+		{[]string{"three"}, expected{"3", true}},
+		{[]string{"five"}, expected{"5", true}},
+		{[]string{"thirtyfive"}, expected{"35", true}},
+		{[]string{"thirty-five"}, expected{"35", true}},
+		{[]string{"foo"}, expected{"", false}},
+		{[]string{"three", "hundred"}, expected{"300", true}},
+		{[]string{"3", "hundred"}, expected{"300", true}},
+		{[]string{"3", "hundred", "million"}, expected{"300000000", true}},
+	}
+
+	for _, test := range tests {
+		result, ok := Dictionary.Lookup(test.input)
+
+		if ok != test.expected.ok {
+			t.Errorf("got ok %v, expected %v, for input %q", ok, test.expected.ok, test.input)
+		}
+		if result != test.expected.result {
+			t.Errorf("got result %q, expected %q, for input %q", result, test.expected.result, test.input)
+		}
+	}
+}
