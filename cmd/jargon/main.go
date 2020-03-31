0cb212a68ab0819cb007177f13e12fcc90beaa48
Matt Sherman <mwsherman@gmail.com>
2020-03-31 16:58:31 -0400

    Flesh out the CLI

--- cmd/jargon/main.go
+++ cmd/jargon/main.go
@@ -1,10 +1,9 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
-	"io"
 	"os"
-	"strings"
 
 	"github.com/clipperhouse/jargon"
 	"github.com/clipperhouse/jargon/ascii"
@@ -14,56 +13,148 @@ import (
 )
 
 func main() {
-	tokenize := jargon.Tokenize
-	var filters []jargon.Filter
+	var config = &config{}
 
 	args := os.Args[1:]
 	for i := 0; i < len(args); i++ {
-		arg := strings.ToLower(args[i])
+		arg := args[i]
 
-		t, ok := tokenizeMap[arg]
-		if ok {
-			tokenize = t
+		if arg == "-html" {
+			config.HTML = true
 			continue
 		}
 
-		f, ok := filterMap[arg]
+		filter, found := filterMap[arg]
+		if found {
+			if arg == "-stem" {
+				// Lookahead for language
+				if len(args) > i+1 {
+					stemlang := args[i+1]
+					stemmer, ok := stemmerMap[stemlang]
+					if ok {
+						filter = stemmer
+						i++
+					}
+				}
+				// Otherwise defaults to English
+			}
 
-		if !ok {
+			config.Filters = append(config.Filters, filter)
+		}
+
+		switch {
+		case i == 0 && !found:
+			// Interpret first arg as input file path
+			file, err := os.Open(arg)
+			if err != nil {
+				unknown := fmt.Errorf("%s is not a valid flag or file", arg)
+				os.Stderr.WriteString(unknown.Error() + "\n")
+			}
+			check(err)
+			defer file.Close()
+
+			config.Filein = file
+		case i == len(args)-1 && !found:
+			// Interpret last arg as output file path
+			file, err := os.Create(arg)
+			if err != nil {
+				unknown := fmt.Errorf("%s is not a valid flag or file", arg)
+				os.Stderr.WriteString(unknown.Error() + "\n")
+			}
+			check(err)
+			defer file.Close()
+
+			config.Fileout = file
+		case !found:
 			err := fmt.Errorf("unknown flag %q", arg)
 			check(err)
 		}
-
-		if arg == "-stem" {
-			// Lookahead for language
-			if len(args) > i+1 {
-				stemlang := args[i+1]
-				stemmer, ok := stemmerMap[stemlang]
-				if ok {
-					f = stemmer
-					i++
-				}
-			}
-			// Otherwise defaults to English
-		}
-
-		filters = append(filters, f)
 	}
 
-	fi, err := os.Stdin.Stat()
+	// Piped?
+	in, err := os.Stdin.Stat()
 	check(err)
+	pipedin := (in.Mode() & os.ModeCharDevice) == 0 // https://stackoverflow.com/a/43947435/70613
 
-	piped := (fi.Mode() & os.ModeCharDevice) == 0 // https://stackoverflow.com/a/43947435/70613
-
-	if piped {
-		tokens := tokenize(os.Stdin)
-		for _, f := range filters {
-			tokens = tokens.Filter(f)
-		}
-
-		_, err := tokens.WriteTo(os.Stdout)
+	if pipedin && config.Filein != nil {
+		err := fmt.Errorf("choose *either* an input file argument or piped input")
 		check(err)
 	}
+
+	noInput := !pipedin && config.Filein == nil
+	if noInput {
+		if len(config.Filters) > 0 {
+			os.Stderr.WriteString("indicate a file path as the first argument, or pipe in text\n")
+		}
+		os.Stderr.WriteString("Usage:\n")
+		return
+	}
+
+	pipedout := config.Fileout == nil
+
+	if pipedin || pipedout {
+		// We're limited by pipe buffer size, typically 64K with back pressure
+		size := 64 * 1024
+		if pipedin {
+			config.Reader = bufio.NewReaderSize(os.Stdin, size)
+		} else {
+			config.Reader = bufio.NewReaderSize(config.Filein, size)
+		}
+	}
+
+	if config.Reader == nil {
+		fi, err := config.Filein.Stat()
+		check(err)
+
+		size := fi.Size()
+		switch {
+		case size <= 4*1024:
+			// Minimum of 4K
+			config.Reader = bufio.NewReaderSize(config.Filein, 4*1024)
+		case size <= 1024*1024:
+			// Aim for a right-sized buffer (single read, perhaps) up to 1MB
+			config.Reader = bufio.NewReaderSize(config.Filein, int(size))
+		default:
+			// Otherwise, use 1MB buffer size, better perf over default, but not huge
+			config.Reader = bufio.NewReaderSize(config.Filein, 1024*1024)
+		}
+	}
+
+	if config.Writer == nil {
+		// Match the input buffer size
+		size := config.Reader.Size()
+		if pipedout {
+			config.Writer = bufio.NewWriterSize(os.Stdout, size)
+		} else {
+			config.Writer = bufio.NewWriterSize(config.Fileout, size)
+		}
+	}
+
+	var tokens *jargon.Tokens
+	if config.HTML {
+		tokens = jargon.TokenizeHTML(config.Reader)
+	} else {
+		tokens = jargon.Tokenize(config.Reader)
+	}
+	for _, f := range config.Filters {
+		tokens = tokens.Filter(f)
+	}
+
+	if _, err := tokens.WriteTo(config.Writer); err != nil {
+		check(err)
+	}
+	if err := config.Writer.Flush(); err != nil {
+		check(err)
+	}
+}
+
+type config struct {
+	Filein  *os.File
+	Fileout *os.File
+	Filters []jargon.Filter
+	HTML    bool
+	Reader  *bufio.Reader
+	Writer  *bufio.Writer
 }
 
 func check(err error) {
@@ -72,10 +163,6 @@ func check(err error) {
 		os.Stderr.WriteString("\n")
 		os.Exit(1)
 	}
-}
-
-var tokenizeMap = map[string](func(io.Reader) *jargon.Tokens){
-	"-html": jargon.TokenizeHTML,
 }
 
 var filterMap = map[string]jargon.Filter{
